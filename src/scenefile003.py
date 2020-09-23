18ff8f590d7e7606d01e6c1f39c99ec624b4e4ed
ss-cyan <sscynthiayan@gmail.com>
2020-09-22 23:33:37 -0500

    Fourth commit, done after watching SceneFile #03

---
+++ src/scenefile003.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+class SceneFile(object):
+    """An abstract representation of a Scene file."""
+    def __init__(self, folder_path, descriptor, task, ver, ext):
+        self.folder_path = Path(folder_path)
+        self.descriptor = descriptor
+        self.task = task
+        self.ver = ver
+        self.ext = ext
+
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task,
+                              ver=self.ver,
+                              ext=self.ext)
+
+    @property
+    def path(self):
+        return self.folder_path / self.filename
+
+
+scene_file = SceneFile("D:\\", "tank", "model", 1, ".ma")
+print(scene_file.path)
