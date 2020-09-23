b6ad29421a12eff5f9322ac8ecdce526fd229e0c
ss-cyan <sscynthiayan@gmail.com>
2020-09-22 23:45:51 -0500

    Fifth and last commit, done after watching SceneFile#04

---
+++ src/scenefile004.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+class SceneFile(object):
+    """An abstract representation of a Scene file."""
+    def __init__(self, path):
+        self.folder_path = Path()
+        self.descriptor = 'main'
+        self.task = None
+        self.ver = 1
+        self.ext = '.ma'
+        self._init_from_path(path)
+
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task,
+                              ver=self.ver,
+                              ext=self.ext)
+
+    @property
+    def path(self):
+        return self.folder_path / self.filename
+
+    def _init_from_path(self, path):
+        path = Path(path)
+        self.folder_path = path.parent
+        self.ext = path.suffix
+        self.descriptor, self.task, ver = path.stem.split("_")
+        self.ver = int(ver.split("v")[-1])
+
+
+scene_file = SceneFile("D:/cynthia's weird projects/bobo_lighting_v201.ma")
+print(scene_file.folder_path)
+print(scene_file.descriptor)
+print(scene_file.task)
+print(scene_file.ver)
+print(scene_file.ext)
+print(scene_file.filename)
